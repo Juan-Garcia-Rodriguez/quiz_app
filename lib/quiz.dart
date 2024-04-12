7da65882d839af927ab103abed13cd177b206c1e
Juan-Garcia-Rodriguez <juangarciarodriguez.dev@gmail.com>
2024-04-12 05:15:07 +0200

    first commit

---
+++ lib/quiz.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:quizz_app/data/questions.dart';
+import 'package:quizz_app/questions_screen.dart';
+import 'package:quizz_app/results_screen.dart';
+import 'package:quizz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = QuestionsScreen(chooseAnswer);
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = ResultsScreen(selectedAnswers, switchScreen);
+        selectedAnswers = [];
+      });
+    }
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
